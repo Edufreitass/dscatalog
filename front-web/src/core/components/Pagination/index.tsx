39e797c61af3e47fa0b073c1035998b7cae6ece4
Eduardo Freitas <eduardoflorencio96@gmail.com>
2020-11-18 11:19:40 -0300

    Finalizando componente de paginação

--- front-web/src/core/components/Pagination/index.tsx
+++ front-web/src/core/components/Pagination/index.tsx
@@ -12,11 +12,15 @@ type Props = {
 
 const Pagination = ({ totalPages, activePage, onChange }: Props) => {
   const items = generateList(totalPages);
-  // [0, 1, 2, 3, 4]
+  const previousClass = totalPages > 0 && activePage > 0 ? 'page-active' : 'page-inactive';
+  const nextClass = (activePage + 1) < totalPages ? 'page-active' : 'page-inactive';
 
   return (
     <div className="pagination-container">
-      <ArrowIcon className="pagination-previous" />
+      <ArrowIcon 
+        className={`pagination-previous ${previousClass}`}
+        onClick={() => onChange(activePage - 1)}
+      />
       {items.map(item => (
         <div 
           key={item}
@@ -26,7 +30,10 @@ const Pagination = ({ totalPages, activePage, onChange }: Props) => {
           {item + 1}
         </div>
       ))}
-      <ArrowIcon className="pagination-next" />
+      <ArrowIcon 
+        className={`pagination-next ${nextClass}`}
+        onClick={() => onChange(activePage + 1)}
+      />
     </div>
   );
 }
