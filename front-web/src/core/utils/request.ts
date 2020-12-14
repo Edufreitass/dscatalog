1dbf55c7e67149ca68c46fb9a9c1afa090908c62
Eduardo Freitas <eduardoflorencio96@gmail.com>
2020-12-14 14:42:23 -0300

    Cadastrando um produto após a autenticação

--- front-web/src/core/utils/request.ts
+++ front-web/src/core/utils/request.ts
@@ -1,41 +1,51 @@
 import axios, { Method } from 'axios';
 import qs from 'qs';
-import { CLIENT_ID, CLIENT_SECRET } from './auth';
+import { CLIENT_ID, CLIENT_SECRET, getSessionData } from './auth';
 
 type RequestParams = {
-    method?: Method;
-    url: string;
-    data?: object | string;
-    params?: object;
-    headers?: object;
+  method?: Method;
+  url: string;
+  data?: object | string;
+  params?: object;
+  headers?: object;
 }
 
 type LoginData = {
-    username: string;
-    password: string;
+  username: string;
+  password: string;
 }
 
 const BASE_URL = 'http://localhost:8080';
 
 export const makeRequest = ({ method = 'GET', url, data, params, headers }: RequestParams) => {
-    return axios({
-        method,
-        url: `${BASE_URL}${url}`,
-        data,
-        params,
-        headers
-    });
+  return axios({
+    method,
+    url: `${BASE_URL}${url}`,
+    data,
+    params,
+    headers
+  });
+}
+
+export const makePrivateRequest = ({ method = 'GET', url, data, params }: RequestParams ) => {
+  const sessionData = getSessionData();
+
+  const headers = {
+    'Authorization': `Bearer ${sessionData.access_token}`
+  }
+
+  return makeRequest({ method, url, data, params, headers });
 }
 
 export const makeLogin = (loginData: LoginData) => {
-    const token = `${CLIENT_ID}:${CLIENT_SECRET}`;
+  const token = `${CLIENT_ID}:${CLIENT_SECRET}`;
 
-    const headers = {
-        Authorization: `Basic ${window.btoa(token)}`,
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
+  const headers = {
+    Authorization: `Basic ${window.btoa(token)}`,
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
 
-    const payload = qs.stringify({ ...loginData, grant_type: 'password' });
+  const payload = qs.stringify({ ...loginData, grant_type: 'password' });
 
-    return makeRequest({ url: '/oauth/token', data: payload, method: 'POST', headers });
+  return makeRequest({ url: '/oauth/token', data: payload, method: 'POST', headers });
 }
