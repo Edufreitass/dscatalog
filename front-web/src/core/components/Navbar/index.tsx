f0d607fdc1d95c81f31c5ffb8da58803a663bc14
Eduardo Freitas <eduardoflorencio96@gmail.com>
2020-12-15 14:57:21 -0300

    Melhorando o componente Navbar para incluir a ação de Login

--- front-web/src/core/components/Navbar/index.tsx
+++ front-web/src/core/components/Navbar/index.tsx
@@ -3,32 +3,35 @@ import { Link, NavLink } from 'react-router-dom';
 import './styles.scss';
 
 const Navbar = () => (
-    <nav className="row bg-primary main-nav">
-        <div className="col-2">
-            <Link to="/" className="nav-logo-text">
-                <h4>DS Catalog</h4>
-            </Link>
-        </div>
-        <div className="col-6 offset-2">
-            <ul className="main-menu">
-                <li>
-                    <NavLink to="/" exact>
-                        HOME
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/products">
-                        CATÁLOGO
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/admin">
-                        ADMIN
-                    </NavLink>
-                </li>
-            </ul>
-        </div>
-    </nav>
+  <nav className="row bg-primary main-nav">
+    <div className="col-3">
+      <Link to="/" className="nav-logo-text">
+        <h4>DS Catalog</h4>
+      </Link>
+    </div>
+    <div className="col-6">
+      <ul className="main-menu">
+        <li>
+          <NavLink to="/" exact>
+            HOME
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/products">
+            CATÁLOGO
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/admin">
+            ADMIN
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+    <div className="col-3 text-right">
+      LOGIN
+    </div>
+  </nav>
 );
 
 export default Navbar;
