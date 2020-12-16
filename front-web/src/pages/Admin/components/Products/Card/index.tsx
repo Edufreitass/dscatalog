ee65b5e7435744011bf871c486f044ddaa0dd5bf
Eduardo Freitas <eduardoflorencio96@gmail.com>
2020-12-16 15:13:57 -0300

    Criando edicão do produto - parte 1

--- front-web/src/pages/Admin/components/Products/Card/index.tsx
+++ front-web/src/pages/Admin/components/Products/Card/index.tsx
@@ -1,6 +1,7 @@
+import React from 'react';
 import ProductPrice from 'core/components/ProductPrice';
 import { Product } from 'core/types/Product';
-import React from 'react';
+import { Link } from 'react-router-dom';
 import './styles.scss';
 
 type Props = {
@@ -24,20 +25,22 @@ const Card = ({ product }: Props) => {
           </h3>
           <ProductPrice price={product.price} />
           <div>
-            {product.categories.map(category => (
-              <span className="badge bg-secondary">
-                {category}
-              </span>
-            ))}
+            <span className="badge badge-pill badge-secondary mr-2">
+              Categoria 1
+            </span>
+            <span className="badge badge-pill badge-secondary mr-2">
+              Categoria 2
+            </span>
           </div>
         </div>
         <div className="col-3 pt-3 pr-5">
-          <button
+          <Link
+            to={`/admin/products/${product.id}`}
             type="button"
             className="btn btn-outline-secondary btn-block border-radius-10 mb-3 btn-edit"
           >
             EDITAR
-          </button>
+          </Link>
           <button
             type="button"
             className="btn btn-outline-danger btn-block border-radius-10"
