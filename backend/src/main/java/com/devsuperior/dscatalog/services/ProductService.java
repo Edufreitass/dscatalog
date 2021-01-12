c48563096a20d1aed2782b54590eae7672bf0c35
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-01-12 15:58:11 -0300

    INNER JOIN, IN

--- backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -31,8 +31,9 @@ public class ProductService {
 	private CategoryRepository categoryRepository;
 
 	@Transactional(readOnly = true)
-	public Page<ProductDTO> findAllPaged(PageRequest pageRequest) {
-		Page<Product> page = repository.findAll(pageRequest);
+	public Page<ProductDTO> findAllPaged(Long categoryId, PageRequest pageRequest) {
+		Category category = (categoryId == 0) ? null : categoryRepository.getOne(categoryId);
+		Page<Product> page = repository.find(category, pageRequest);
 		return page.map(x -> new ProductDTO(x));
 	}
 
