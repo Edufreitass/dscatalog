46389c7bb94279677a69df1ba1305fa735c48379
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-01-22 10:56:43 -0300

    Modificando o ProductFactory

--- backend/src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
@@ -64,6 +64,10 @@ public class ProductDTO implements Serializable {
 	public Long getId() {
 		return id;
 	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getName() {
 		return name;
