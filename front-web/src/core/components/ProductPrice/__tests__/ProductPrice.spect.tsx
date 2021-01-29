2c4bd423f7431cfbc7bf88754b270e0b61299889
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-01-29 10:36:40 -0300

    Testando o primeiro componente React PARTE 2

--- front-web/src/core/components/ProductPrice/__tests__/ProductPrice.spect.tsx
+++ front-web/src/core/components/ProductPrice/__tests__/ProductPrice.spect.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { render, screen } from "@testing-library/react";
+import ProductPrice from '..'
 
 test('should render ProductPrice', () => {
   // Arrange
-  
+  const price = 1200;
 
   // Act
-
+  render(
+    <ProductPrice price={price} />
+  );
 
   // Assert
-
-})
+  // screen.debug();
+  const currencyElement = screen.getByText('R$');
+  const priceElement = screen.getByText('1,200.00');
+  
+  expect(currencyElement).toBeInTheDocument();
+  expect(priceElement).toBeInTheDocument();
+});
