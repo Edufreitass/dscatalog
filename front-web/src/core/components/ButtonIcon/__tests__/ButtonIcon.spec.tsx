037679b7da6a34650d34dd3b5c5d4a2227e0f84c
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-01-29 15:37:14 -0300

    Testando o componente ButtonIcon e aprendendo mais sobre o Jest

---
+++ front-web/src/core/components/ButtonIcon/__tests__/ButtonIcon.spec.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render, screen } from "@testing-library/react";
+import ButtonIcon from '..';
+
+test('should render ButtonIcon', () => {
+  const text = 'logar';
+
+  render(
+    <ButtonIcon text={text} />
+  );
+
+  const textElement = screen.getByText(text);
+  expect(textElement).toBeInTheDocument();
+});
---
+++ front-web/src/core/components/ProductPrice/__tests__/ProductPrice.spec.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { render, screen } from "@testing-library/react";
+import ProductPrice from '..'
+
+describe('Tests with price greather than zero', () => {
+
+  test('should render ProductPrice', () => {
+    // Arrange
+    const price = 1200;
+
+    // Act
+    render(
+      <ProductPrice price={price} />
+    );
+
+    // Assert
+    // screen.debug();
+    const currencyElement = screen.getByText('R$');
+    const priceElement = screen.getByText('1,200.00');
+
+    expect(currencyElement).toBeInTheDocument();
+    expect(priceElement).toBeInTheDocument();
+  });
+
+  test('should render ProductPrice without thousand separator', () => {
+    render(
+      <ProductPrice price={100} />
+    );
+
+    const currencyElement = screen.getByText('R$');
+    const priceElement = screen.getByText('100.00');
+
+    expect(currencyElement).toBeInTheDocument();
+    expect(priceElement).toBeInTheDocument();
+  });
+
+})
+
+it('should render ProductPrice with price equals zero', () => {
+  render(
+    <ProductPrice price={0} />
+  );
+
+  const currencyElement = screen.getByText('R$');
+  const priceElement = screen.getByText('0.00');
+
+  expect(currencyElement).toBeInTheDocument();
+  expect(priceElement).toBeInTheDocument();
+});
