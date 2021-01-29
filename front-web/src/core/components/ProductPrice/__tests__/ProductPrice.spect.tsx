1b5a991cb25b9fa3722feeb4cba2f240e9cbff7a
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-01-29 14:53:41 -0300

    Add ProductPrice tests

--- front-web/src/core/components/ProductPrice/__tests__/ProductPrice.spect.tsx
+++ front-web/src/core/components/ProductPrice/__tests__/ProductPrice.spect.tsx
@@ -19,3 +19,27 @@ test('should render ProductPrice', () => {
   expect(currencyElement).toBeInTheDocument();
   expect(priceElement).toBeInTheDocument();
 });
+
+test('should render ProductPrice with price equals zero', () => {
+  render(
+    <ProductPrice price={0} />
+  );
+
+  const currencyElement = screen.getByText('R$');
+  const priceElement = screen.getByText('0.00');
+  
+  expect(currencyElement).toBeInTheDocument();
+  expect(priceElement).toBeInTheDocument();
+});
+
+test('should render ProductPrice without thousand separator', () => {
+  render(
+    <ProductPrice price={100} />
+  );
+
+  const currencyElement = screen.getByText('R$');
+  const priceElement = screen.getByText('100.00');
+  
+  expect(currencyElement).toBeInTheDocument();
+  expect(priceElement).toBeInTheDocument();
+});
