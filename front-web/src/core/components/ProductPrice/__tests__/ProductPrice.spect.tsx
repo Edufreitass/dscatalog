e5840d664732d2ed10753bebda10a794276f5616
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-01-29 10:19:25 -0300

    Testando o primeiro componente React PARTE 1

---
+++ front-web/src/core/components/ProductPrice/__tests__/ProductPrice.spect.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render, screen } from "@testing-library/react";
+
+test('should render ProductPrice', () => {
+  // Arrange
+  
+
+  // Act
+
+
+  // Assert
+
+})
