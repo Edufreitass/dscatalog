d25c102a993dfbdac70d67f37d7aad1ea5c699e7
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-02-04 12:02:01 -0300

    Testando o componente Home

--- front-web/src/pages/Home/index.tsx
+++ front-web/src/pages/Home/index.tsx
@@ -19,7 +19,7 @@ const Home = () => (
                 </Link>
             </div>
             <div className="col-6">
-                <MainImage className="main-image"/>
+                <MainImage className="main-image" data-testid="main-image" />
             </div>
         </div>
     </div>
