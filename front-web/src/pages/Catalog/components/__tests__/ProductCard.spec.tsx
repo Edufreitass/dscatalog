aa269fbbc42ca9e6d9a06d691310d5cae2a8fb5b
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-02-04 15:26:26 -0300

    Add ProductCard tests

---
+++ front-web/src/pages/Catalog/components/__tests__/ProductCard.spec.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render, screen } from "@testing-library/react";
+import ProductCard from '../ProductCard';
+import { Product } from 'core/types/Product';
+
+test('should render ProductCard', () => {
+  // Arrange
+  const product = {
+    name: 'computador',
+    imgUrl: 'image.jpg',
+    price: 40
+  } as Product;
+
+  // Act
+  render(
+    <ProductCard product={product} />
+  );
+
+  // Assert
+  expect(screen.getByText('computador')).toBeInTheDocument();
+  expect(screen.getByAltText('computador')).toBeInTheDocument();
+  expect(screen.getByText('R$')).toBeInTheDocument();
+  expect(screen.getByText('40.00')).toBeInTheDocument();
+});
