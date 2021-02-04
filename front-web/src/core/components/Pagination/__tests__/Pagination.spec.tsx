021edec6024d20e30e6514dddb4086c62a57ba94
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-02-04 11:31:58 -0300

    Testando o componente de paginação - PARTE 2

--- front-web/src/core/components/Pagination/__tests__/Pagination.spec.tsx
+++ front-web/src/core/components/Pagination/__tests__/Pagination.spec.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
 import Pagination from '..';
 
 test('should render Pagination', () => {
@@ -34,4 +35,59 @@ test('should render Pagination', () => {
   expect(secondPageItem).not.toHaveClass('active');
   expect(thirdPageItem).toBeInTheDocument();
   expect(thirdPageItem).not.toHaveClass('active');
+});
+
+test('should enable previous action and disable the next action', () => {
+  // Arrange
+  const totalPages = 3;
+  const activePage = 2;
+  const onChange = () => null;
+
+  // Act
+  render(
+    <Pagination
+      totalPages={totalPages}
+      activePage={activePage}
+      onChange={onChange}
+    />
+  );
+
+  // Assert
+  const previousElement = screen.getByTestId('arrow-icon-previous');
+  const nextElement = screen.getByTestId('arrow-icon-next');
+
+  expect(previousElement).toBeInTheDocument();
+  expect(previousElement).toHaveClass('page-active');
+  expect(nextElement).toBeInTheDocument();
+  expect(nextElement).toHaveClass('page-inactive');
+});
+
+test('should trigger onChange action', () => {
+  // Arrange
+  const totalPages = 3;
+  const activePage = 1;
+  const onChange = jest.fn();
+
+  // Act
+  render(
+    <Pagination
+      totalPages={totalPages}
+      activePage={activePage}
+      onChange={onChange}
+    />
+  );
+
+  // Assert
+  const previousElement = screen.getByTestId('arrow-icon-previous');
+  const nextElement = screen.getByTestId('arrow-icon-next');
+  const firstPageItem = screen.getByText('1');
+
+  userEvent.click(firstPageItem);
+  expect(onChange).toHaveBeenCalledWith(0);
+
+  userEvent.click(previousElement);
+  expect(onChange).toHaveBeenCalledWith(0);
+
+  userEvent.click(nextElement);
+  expect(onChange).toHaveBeenCalledWith(2);
 });
