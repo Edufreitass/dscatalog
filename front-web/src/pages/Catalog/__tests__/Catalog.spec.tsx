866dd5f07e549aead016837b4e32664f1ac92c36
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-02-08 17:50:59 -0300

    Testando a listagem de produtos do catálogo - parte 3

--- front-web/src/pages/Catalog/__tests__/Catalog.spec.tsx
+++ front-web/src/pages/Catalog/__tests__/Catalog.spec.tsx
@@ -3,32 +3,9 @@ import { render, screen, waitFor } from "@testing-library/react";
 import { Router } from 'react-router-dom';
 import { rest } from 'msw'
 import { setupServer } from 'msw/node'
+import { productsResponse } from './fixtures';
 import history from 'core/utils/history';
 import Catalog from '..';
-
-const productsResponse = {
-  "content": [
-    {
-      "id": 3,
-      "name": "Macbook Pro",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      "price": 1250.0,
-      "imgUrl": "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg",
-      "date": "2020-07-14T10:00:00Z",
-      "categories": []
-    },
-    {
-      "id": 1,
-      "name": "Notebook Pro",
-      "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-      "price": 2000.0,
-      "imgUrl": "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/6-big.jpg",
-      "date": null,
-      "categories": []
-    }
-  ],
-  "totalPages": 2
-};
 
 const server = setupServer(
   rest.get('http://localhost:8080/products', (req, res, ctx) => {
