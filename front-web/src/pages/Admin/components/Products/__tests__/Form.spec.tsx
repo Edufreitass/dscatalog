bdb761468de0eb7e24a9a716d4b0b1b241d94105
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-02-11 13:10:10 -0300

    Testando a criação de produtos - PARTE 3

--- front-web/src/pages/Admin/components/Products/__tests__/Form.spec.tsx
+++ front-web/src/pages/Admin/components/Products/__tests__/Form.spec.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from '@testing-library/user-event';
 import selectEvent from 'react-select-event';
 import { Router } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import { rest } from 'msw'
 import { setupServer } from 'msw/node'
 import history from 'core/utils/history';
@@ -19,6 +20,9 @@ jest.mock('react-router-dom', () => ({
 const server = setupServer(
   rest.get('http://localhost:8080/categories', (req, res, ctx) => {
     return res(ctx.json(categoriesResponse));
+  }),
+  rest.post('http://localhost:8080/products', (req, res, ctx) => {
+    return res(ctx.status(201));
   })
 )
 
@@ -29,6 +33,7 @@ afterAll(() => server.close());
 test('should render Form', async () => {
   render(
     <Router history={history}> 
+      <ToastContainer />
       <Form />
     </Router>
   );
@@ -47,4 +52,8 @@ test('should render Form', async () => {
   userEvent.type(descriptionInput, 'ótimo computador');
 
   userEvent.click(submitButton);
+
+  await waitFor(() => expect(screen.getByText('Produto salvo com sucesso!')).toBeInTheDocument());
+  expect(history.location.pathname).toBe('/admin/products');
+  expect(screen.getByText(/CADASTRAR UM PRODUTO/i)).toBeInTheDocument();
 });
