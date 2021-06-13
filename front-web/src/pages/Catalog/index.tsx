667121c21ed80896671d94a36a2bf6a153ee111c
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-06-13 16:12:40 -0300

    03 Integrando o filtro de nome com o backend

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { ProductsResponse } from 'core/types/Product';
 import { makeRequest } from 'core/utils/request';
-import ProductFilters from 'core/components/ProductFilters';
+import ProductFilters, { FilterForm } from 'core/components/ProductFilters';
 import ProductCard from './components/ProductCard';
 import ProductCardLoader from './components/Loaders/ProductCardLoader';
 import Pagination from 'core/components/Pagination';
@@ -13,10 +13,12 @@ const Catalog = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [activePage, setActivePage] = useState(0);
 
-  useEffect(() => {
+  const getProducts = useCallback((filter?: FilterForm) => {
     const params = {
       page: activePage,
-      linesPerPage: 12
+      linesPerPage: 12,
+      name: filter?.name,
+      categoryId: filter?.categoryId
     }
 
     setIsLoading(true);
@@ -27,13 +29,17 @@ const Catalog = () => {
       })
   }, [activePage]);
 
+  useEffect(() => {
+    getProducts();
+  }, [getProducts]);
+
   return (
     <div className="catalog-container">
       <div className="d-flex justify-content-between">
         <h1 className="catalog-title">
           Catálogo de produtos
         </h1>
-        <ProductFilters />
+        <ProductFilters onSearch={filter => getProducts(filter)} />
       </div>
       <div className="catalog-products">
         {isLoading ? <ProductCardLoader /> : (
@@ -45,11 +51,11 @@ const Catalog = () => {
         )}
       </div>
       {productsResponse && (
-      <Pagination
-        totalPages={productsResponse.totalPages}
-        activePage={activePage}
-        onChange={page => setActivePage(page)}
-      />
+        <Pagination
+          totalPages={productsResponse.totalPages}
+          activePage={activePage}
+          onChange={page => setActivePage(page)}
+        />
       )}
     </div>
   );
