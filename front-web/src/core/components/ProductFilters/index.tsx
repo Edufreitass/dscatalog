7bed15483be69278e440db79e3d5527efdc7496b
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-06-13 15:38:48 -0300

    02 Criando layout do filtro de produtos - PARTE 2

--- front-web/src/core/components/ProductFilters/index.tsx
+++ front-web/src/core/components/ProductFilters/index.tsx
@@ -1,18 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ReactComponent as SearchIcon } from 'core/assets/images/search-icon.svg';
+import Select from 'react-select';
 import './styles.scss';
+import { makeRequest } from 'core/utils/request';
+import { Category } from 'core/types/Product';
 
 const ProductFilters = () => {
+  const [isLoadingCategories, setIsLoadingCategories] = useState(false);
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  useEffect(() => {
+    setIsLoadingCategories(true);
+    makeRequest({ url: '/categories' })
+      .then(response => setCategories(response.data.content))
+      .finally(() => setIsLoadingCategories(false))
+  }, []);
+
   return (
     <div className="card-base product-filters-container">
       <div className="input-search">
-        <input 
+        <input
           type="text"
           className="form-control"
           placeholder="Pesquisar produto"
         />
         <SearchIcon />
       </div>
+      <Select
+        name="categories"
+        isLoading={isLoadingCategories}
+        options={categories}
+        getOptionLabel={(option: Category) => option.name}
+        getOptionValue={(option: Category) => String(option.id)}
+        className="filter-select-container"
+        classNamePrefix="product-categories-select"
+        placeholder="Categorias"
+        inputId="categories"
+      />
+      <button
+        className="btn btn-outline-secondary border-radius-10"
+      >
+        <strong>LIMPAR FILTRO</strong>
+      </button>
     </div>
   )
 }
