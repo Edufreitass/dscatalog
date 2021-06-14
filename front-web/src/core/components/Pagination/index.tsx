b09c9eb1bdcaee63ecc6acdcd475d65146bc78c3
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-06-14 20:55:19 -0300

    Recurso ... (3 pontinhos) no componente de paginação PARTE 1

--- front-web/src/core/components/Pagination/index.tsx
+++ front-web/src/core/components/Pagination/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { ReactComponent as ArrowIcon } from 'core/assets/images/arrow.svg';
-import { generateList } from 'core/utils/list';
-
+import ReactPaginate from 'react-paginate';
 import './styles.scss';
 
 type Props = {
@@ -11,30 +10,20 @@ type Props = {
 }
 
 const Pagination = ({ totalPages, activePage, onChange }: Props) => {
-  const items = generateList(totalPages);
-  const previousClass = totalPages > 0 && activePage > 0 ? 'page-active' : 'page-inactive';
-  const nextClass = (activePage + 1) < totalPages ? 'page-active' : 'page-inactive';
+  const renderIcon = () => (
+    <ArrowIcon
+      className={`pagination-previous`}
+      data-testid="arrow-icon-previous"
+    />
+  );
 
   return (
     <div className="pagination-container">
-      <ArrowIcon 
-        className={`pagination-previous ${previousClass}`}
-        onClick={() => onChange(activePage - 1)}
-        data-testid="arrow-icon-previous"
-      />
-      {items.map(item => (
-        <div 
-          key={item}
-          className={`pagination-item ${item === activePage ? 'active' : ''}`}
-          onClick={() => onChange(item)}
-        >
-          {item + 1}
-        </div>
-      ))}
-      <ArrowIcon 
-        className={`pagination-next ${nextClass}`}
-        onClick={() => onChange(activePage + 1)}
-        data-testid="arrow-icon-next"
+      <ReactPaginate
+        pageCount={totalPages}
+        pageRangeDisplayed={5}
+        marginPagesDisplayed={1}
+        onPageChange={selectedItem => onChange(selectedItem.selected)}
       />
     </div>
   );
