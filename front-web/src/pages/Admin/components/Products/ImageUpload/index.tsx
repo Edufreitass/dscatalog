e434cc41834a4a6a65db6c465a5de9d8e5d49a7f
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-06-16 20:14:20 -0300

    03 Realizando upload da imagem para o servidor

--- front-web/src/pages/Admin/components/Products/ImageUpload/index.tsx
+++ front-web/src/pages/Admin/components/Products/ImageUpload/index.tsx
@@ -1,8 +1,36 @@
 import React from "react";
 import { ReactComponent as UploadPlaceholder } from 'core/assets/images/upload-placeholder.svg';
 import './styles.scss';
+import { makePrivateRequest } from "core/utils/request";
 
 const ImageUpload = () => {
+
+  const uploadImage = (selectedImage: File) => {
+    const payload = new FormData();
+    payload.append('file', selectedImage);
+
+    makePrivateRequest({
+      url: '/products/image',
+      method: 'POST',
+      data: payload
+    })
+      .then(() => {
+        console.log("arquivo enviado com sucesso");
+      })
+      .catch(() => {
+        console.log("Erro ao enviar arquivo");
+
+      })
+  }
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedImage = event.target.files?.[0];
+
+    if (selectedImage) {
+      uploadImage(selectedImage);
+    }
+  }
+
   return (
     <div className="row">
       <div className="col-6">
@@ -10,6 +38,8 @@ const ImageUpload = () => {
           <input
             type="file"
             id="upload"
+            accept="image/png, image/jpg"
+            onChange={handleChange}
             hidden
           />
           <label htmlFor="upload">ADICIONAR IMAGEM</label>
