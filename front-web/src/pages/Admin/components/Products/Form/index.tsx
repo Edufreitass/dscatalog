d2555bb1123d490813d4bea1b3e126114dfbf1f4
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-06-16 21:10:24 -0300

    07 Resolvendo bug do upload de imagem na edição de um produto

--- front-web/src/pages/Admin/components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/components/Products/Form/index.tsx
@@ -56,7 +56,7 @@ const Form = () => {
   const onSubmit = (data: FormState) => {
     const payload = {
       ...data,
-      imgUrl: uploadedImgUrl
+      imgUrl: uploadedImgUrl || productImgUrl
     }
 
     makePrivateRequest({
